2651121ed54b92831ea50e5ecb9c0bddbd5fb9b2
XiaoLFeng <gm@x-lf.cn>
2023-12-29 16:09:01 +0800

    初始化提交

---
+++ src/main/java/org/xlf/function/resourcesdocking/SocialResourceDockingApplication.java
@@ -0,0 +1,13 @@
+package org.xlf.function.resourcesdocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialResourceDockingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialResourceDockingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/xlf/function/resourcesdocking/SocialResourceDockingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xlf.function.resourcesdocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialResourceDockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
